1d017606dbbbcc5b8e9a0a0d41efaff3552eabb5
rupert-ma <michael.a.rupert@gmail.com>
2022-06-20 09:44:47 -0700

    tested api call in app.js

--- musiclibraryreact/src/App.js
+++ musiclibraryreact/src/App.js
@@ -1,11 +1,30 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  
+  const [songs, setSongs]=useState([]);
+
+  useEffect(()=>{
+    getAllSongs();
+  },[])
+
+  async function getAllSongs(){
+    try{
+      let response = await axios.get('http://127.0.0.1:8000/music/');
+      console.log(response.data);
+    } catch (ex) {
+      console.log('Error in getAllSongs API Call!')
+    }
+    
+    // setSongs(response.data)
+  }
+  
   return (
     <div>
-     <h3>Hello world</h3>
+     <button onClick={getAllSongs}>Click</button>
     </div>
-  );
+  )
 }
 
 export default App;
